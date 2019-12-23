2e11d05d50c3f6434d755f070945f35f722d45ce
Kundai Clayton Chatambudza <chtkun003@myuct.ac.za>
2019-12-23 16:17:49 +0200

    Updates

---
+++ precache-manifest.4aee6b67af73ed512f7196369db5c1f9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8f154c9ae8ec19adc9bc2824751e3a",
+    "url": "/property2424/index.html"
+  },
+  {
+    "revision": "5aece45fc43bd5cfdef3",
+    "url": "/property2424/static/css/2.c00f9245.chunk.css"
+  },
+  {
+    "revision": "ffa9836bbdb36a8402a3",
+    "url": "/property2424/static/css/main.f6eec45c.chunk.css"
+  },
+  {
+    "revision": "5aece45fc43bd5cfdef3",
+    "url": "/property2424/static/js/2.5c71fd25.chunk.js"
+  },
+  {
+    "revision": "ffa9836bbdb36a8402a3",
+    "url": "/property2424/static/js/main.4a574700.chunk.js"
+  },
+  {
+    "revision": "0c90779c15351bf4da66",
+    "url": "/property2424/static/js/runtime-main.d0becb26.js"
+  },
+  {
+    "revision": "4ff795a8aaa0b9bbd8862853bce9fa33",
+    "url": "/property2424/static/media/logo.4ff795a8.png"
+  },
+  {
+    "revision": "78b4180e33ce23e0fb9944346c85ecbf",
+    "url": "/property2424/static/media/map.78b4180e.png"
+  },
+  {
+    "revision": "cd252202fb924248e9af612d2a46a1ed",
+    "url": "/property2424/static/media/property2.cd252202.jpeg"
+  }
+]);
